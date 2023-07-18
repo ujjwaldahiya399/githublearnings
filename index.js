85bee6649d2e6dd35a0a742171b45d8a2a2c3940
Ujjwal dahiya <ujjwaldahiya350@gmail.com>
2023-07-18 13:56:24 +0530

    fourth-commit

--- index.js
+++ index.js
@@ -10,3 +10,12 @@ thirdLiEle[2].style.backgroundColor = "green"
 for(let i = 0; i < thirdLiEle.length; i++) {
     thirdLiEle[i].style.fontWeight = "bold";
 }
+let newLiEle = document.createElement("li");
+newLiEle.innerText = "Item added"
+let ulEle = document.querySelector("#items");
+ulEle.appendChild(newLiEle);
+let newAddedLiEle = document.getElementsByClassName("list-group-item")[4];
+console.log(newAddedLiEle);
+let newAddedLiEleWithTag = document.getElementsByTagName("li")[4];
+console.log(newAddedLiEleWithTag);
+newAddedLiEleWithTag.style.fontWeight = "bold";
