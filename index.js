661af9c97ab3f4d8a75033525063050b0ad3dc1b
Ujjwal dahiya <ujjwaldahiya350@gmail.com>
2023-07-19 13:18:05 +0530

    sixth-commit

--- index.js
+++ index.js
@@ -27,3 +27,42 @@ let allOddLi = document.querySelectorAll(":nth-child(odd)");
 allOddLi.forEach((li) => {
     li.style.backgroundColor = "green";
 })
+let parentDiv = document.querySelector("#header-title");
+// console.log(parentDiv.parentElement);
+// parentDiv.parentElement
+parentDiv.parentElement.style.backgroundColor = "white";
+
+let lastElementChild = document.querySelector("#items").lastElementChild; //The property only considers element nodes.
+// console.log(lastElementChild);
+lastElementChild.style.backgroundColor = "blue";
+
+let lastChild = document.querySelector("#items").lastChild; // This property represents the last child node of the parent, regardless of whether it is an element node, text node, comment node, etc.
+lastChild.style.backgroundColor = "red";
+
+let firstElementChild = document.querySelector("#items").firstElementChild;
+firstElementChild.style.backgroundColor = "magenta"; 
+
+let firstChildEle = document.querySelector("#items").firstChild; // Gave text node
+firstChildEle.innerText = "green";
+// console.log(firstChildEle);
+
+let nextSiblibing = document.querySelector("#items").nextSibling; // Gave text node
+// console.log(nextSiblibing);
+
+let nextElementSiblng = document.querySelector(".list-group-item").nextElementSibling;
+nextElementSiblng.style.backgroundColor = "skyblue";
+
+let previousSblng = document.querySelectorAll(".list-group-item")[2].previousSibling; // text node.
+console.log(previousSblng)
+
+let previousSblngEle = document.querySelectorAll(".list-group-item")[2].previousElementSibling; // element node
+// console.log(previousSblngEle)
+previousSblngEle.style.backgroundColor = "green"
+
+
+let selectedLi = document.querySelector(".list-group-item");
+selectedLi.setAttribute("id","selected")
+console.log(selectedLi)
+
+let createdTextnode = document.createTextNode("this is a text node");
+selectedLi.appendChild(createdTextnode);
